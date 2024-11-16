0ac3bd554386c54554130621756f4fc5f4de9c70
Areeba Irfan <161799302+Areeba-irfan-Github-account@users.noreply.github.com>
2024-11-17 01:42:11 +0500

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+import Navbar from "./components/Navbar";
+import Books from "./components/Book";
+import Footer from "./components/Footer";
+
+export default function Home() {
+ return(
+   <>
+   <Navbar/>
+   <Books/>
+   <Footer/>
+   </>
+ )
+}
